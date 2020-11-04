fe772933bae2beb4250d30d58694bd9d39dcf879
Leonvdk <leonvandeklundert@gmail.com>
2020-11-04 10:38:02 +0000

    uncommented bootstrap and CSS in index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
-// import 'bootstrap/dist/css/bootstrap.css';
-// Put any other imports below so that CSS from your
-// components takes precedence over default styles.
-// import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
 
 
 ReactDOM.render(
