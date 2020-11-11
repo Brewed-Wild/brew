b517d17732d160cdea327d3b9b321e806872ce75
Leonvdk <leonvandeklundert@gmail.com>
2020-11-11 10:52:49 +0000

    made some style changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -24,22 +24,22 @@ class Footer extends React.Component {
     return (
       <footer className="d-grid bg-light">
         <div className="row row-cols-md-3 row-cols-2">
-          <Link exact to="/" className="logo-footer col ">
-            <img src={LogoText} alt="Brewer's Heaven" />
+          <Link exact to="/" className="col d-flex align-items-center justify-content-center">
+            <img src={LogoText} alt="Brewer's Heaven" className="h-50"/>
           </Link>
-          <div className="social-icons col d-flex d-md-inline flex-column align-items-center">
+          <div className="social-icons col d-flex flex-column flex-md-row align-items-md-center justify-content-md-center">
             <img src={Twitter} alt="" className="social-icon pt-10" />
             <img src={Facebook} alt="" className="social-icon pt-10" />
             <img src={Instagram} alt="" className="social-icon" />
           </div>
           <form className="d-none d-md-flex flex-column col">
-            <input className="form-control" type="email" id="userEmail" name="userEmail" value={this.state.userEmail} placeholder="Email" onChange={this.handleChange} />
+            <input className="form-control text-justify" type="email" id="userEmail" name="userEmail" value={this.state.userEmail} placeholder="Email" onChange={this.handleChange} />
             <input className="form-control" type="text" id="userMessage" name="userMessage" value={this.state.userMessage} onChange={this.handleChange} placeholder="Message"/>
             <input type="submit" className="invisible" onSubmit={this.handleSubmit} />
+            <i>Rua do Conde de Redondo 145-2 andar, 1150-104 Lisboa Phone: +351 910 600 569</i>
           </form>
         </div>
-        <div >
-          <i>Rua do Conde de Redondo 145-2 andar, 1150-104 Lisboa Phone: +351 910 600 569</i>
+        <div className="p-30">
           <hr />
           <i>Brewer's Heaven @ 2020</i>
         </div>
