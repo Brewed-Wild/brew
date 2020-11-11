bef74c6ff614a8258d920466f874ea1cf4813ab5
Leonvdk <leonvandeklundert@gmail.com>
2020-11-11 16:00:54 +0000

    footer is done for laptop screens, but still breaks on smaller screens

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -25,32 +25,33 @@ class Footer extends React.Component {
 
   render() {
     return (
-      <footer className="d-grid bg-light h-100">
-        <div className="row row-cols-sm-3 d-flex align-items-center">
-          <div className="col-2  d-none d-md-flex align-items-center justify-content-center">
-            <Link exact to="/" className="">
+      <footer className="d-grid bg-light h-100 ">
+        <div className="row d-flex align-items-center justify-content-between flex-md-row">
+          <div className="col-2.5  d-none d-md-flex align-items-center ml-5" id="logo-container">
+            <Link exact to="/" className="ml-0">
               <img src={Logo} alt="Brewer's Heaven" className="logo-footer" />
             </Link>
           </div>
-          <div className=" col-3 d-flex flex-column justify-content-start flex-md-row align-items-md-center justify-content-md-center social-icons">
+          <div className=" col d-flex flex-column flex-md-row justify-content-start justify-content-md-center" id="social-icons">
             <a href= "http://www.twitter.com"><img src={Twitter} alt="" className="social-icon m-3" /></a>
             <a href= "http://www.facebook.com"><img src={Facebook} alt="" className="social-icon m-3" /></a>
             <a href= "http://www.instagram.com"><img src={Instagram} alt="" className="social-icon m-3" /></a>
           </div>
-          <div className="col">
+          <div className="col d-flex align-self-start justify-content-center" id="our-partners">
             <p>
               <h6 className="">Our Partners</h6>
-              <div className="partner-logo-container d-inline-grid">
+              <div className="partner-logo-container d-inline-grid justify-self-center">
 
                 <a href="https://www.raspberrypi.org/"><img className= "easytech-logo" src={Easytech} alt="" /></a>
-                <a href="https://handyman.com/"><img className= "fixee-logo" src={Fixee} alt="" /></a>
-                <a href="https://magicseaweed.com/"><img className= "sali-logo" src={SaliSali} alt="" /></a>
+                <a href=""></a><img className= "fixee-logo" src={Fixee} alt="" />
+                {/* <img className="wild-logo" src="https://res.cloudinary.com/wildcodeschool/image/upload/c_fill,h_50/v1/static/irjoy97aq0eol8bf6959" /> */}
+                <img className= "sali-logo" src={SaliSali} alt="" />
               </div>
 
             </p>
           </div>
-          <div className="col d-md-flex flex-column">
-            <p className="text-left">
+          <div className="col-3.5 d-md-flex flex-column mr-2" id="contact-us">
+            <p className="text-right mr-4">
               <h6>Start a conversation</h6>
               <p>
                 <a href="mailto:hi@brewersheaven.com" target="_blank" rel="noopener noreferrer" className="text-dark">hi@brewersheaven.com</a> <br />
@@ -58,14 +59,10 @@ class Footer extends React.Component {
                 <a href="https://www.google.com/maps/place/Rua+do+Conde+de+Redondo+145,+1150-294+Lisboa/@38.7248464,-9.1481597,17z/data=!3m1!4b1!4m5!3m4!1s0xd193377dbff9c5d:0xeb7c8841e775e3ae!8m2!3d38.7248464!4d-9.145971" className="text-dark">Rua do Conde de Redondo 145-2 Lisbon</a>
               </p>
             </p>
-            {/* <input className="form-control text-justify" type="email" id="userEmail" name="userEmail" value={this.state.userEmail} placeholder="Email" onChange={this.handleChange} />
-            <input className="form-control" type="text" id="userMessage" name="userMessage" value={this.state.userMessage} onChange={this.handleChange} placeholder="Message"/>
-            <input type="submit" className="invisible" onSubmit={this.handleSubmit} /> */}
-
           </div>
         </div>
-        <div className=" text-right">
-          <hr />
+        <div className="">
+          <hr className="mt-0"/>
           <i>Brewer's Heaven @ 2020</i>
         </div>
 
