f131b72959d6e9eb71a043cd06d529970d3e3db5
JoaoIsabel <joao.alves.isabel@gmail.com>
2020-11-13 11:36:38 +0000

    Co-authored-by: Leandro Baseggio <leandrobaseggio@gmail.com>

--- src/components/RecipesPage/FontExample.js
+++ src/components/RecipesPage/FontExample.js
@@ -3,35 +3,29 @@
 import React from "react";
 import "./beerCard.css";
 
-function BeerCard(props) {
+function FontExample(props) {
   // eslint-disable-next-line camelcase
   const { name, image_url, tagline, description, abv, ibu, ebc } = props;
 
   return (
-<<<<<<< HEAD
-    <div>
-      <h1>{name}</h1>
-      <img className="imgBeer" src={imageUrl} alt="" />
-=======
     <div className="cardContainer">
       <div className="product-img">
         <img src={image_url} height="60%" alt="" />
       </div>
       <div className="product-info">
         <div className="product-text">
-          <h1>{name}</h1>
-          <h2>{tagline}</h2>
-          <p>{description}</p>
+          <h1>{name} "Kanit" 28px weight 500</h1>
+          <h2>{tagline} "Raleway" 15px letter-spacing: 0.1em</h2>
+          <p>{description} "Cabin" 15px weight: lighter</p>
         </div>
         <div className="beerIndicators">
-          <span>abv:{abv}</span>
+          <span>abv:{abv}"Do Hyeon" 16px</span>
           <span>ibu:{ibu}</span>
           <span>ebc:{ebc}</span>
         </div>
       </div>
->>>>>>> develop
     </div>
   );
 }
 
-export default BeerCard;
+export default FontExample;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
-// import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css';
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
 // import './index.css';
