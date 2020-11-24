cdd2bdbc0f9e1e7f847324435b839fd668ea9235
Leonvdk <leonvandeklundert@gmail.com>
2020-11-24 11:54:02 +0000

    cleaned up folders

---
+++ src/components/RecipesPage/SearchField.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SearchField (props) {
+
+    return(
+        <div>
+           <p>Search by name</p>
+        </div>
+    )
+}
+
+export default SearchField;
