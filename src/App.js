26082eb8524426a6f5293d5558c2395f73ee72da
Leandro Baseggio <leandrobaseggio@gmail.com>
2020-11-26 13:20:46 +0000

    ABOUT  //  LEANDRO  //  DONE: Minor corrections on style and diplay.  Working properly. Responsiveness OK

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
-import HomePage from './components/HomePage/HomePage';
-import BeerList from './components/RecipesPage/BeerList';
-import About from './components/AboutPage/AboutPage';
-import Footer from './components/Footer/Footer';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import About from "./components/AboutPage/AboutPage";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/beerlist" component={BeerList} />
-        <Route path="/about" component={About} />
-      </Switch>
-      <Footer />
+    <div>
+      <About />
     </div>
-
   );
 }
 
--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
-import './AboutPage.css';
+import React from "react";
+import { useHistory } from "react-router-dom";
+import "./AboutPage.css";
 
 function About() {
   const history = useHistory();
   const redirect = () => {
-    history.push('/beerlist');
+    history.push("/beerlist");
   };
 
   return (
-
-    <div className="container-sm">
+    <div className="container-sm" id="cardsContainer">
       <div className="col-sm-10 mx-auto">
-        <h2 className=" font-weight-bold text-center mt-5 "> About Us</h2>
+        <h2 id="titleAboutUs" className=" font-weight-bold text-center">
+          About Us
+        </h2>
         <br />
-        <div className="ourStoryContainer col-sm-12 text-justify p-5 ml-1 my-5">
+        <div
+          id="cardOne"
+          className="ourStoryContainer col-sm-12 text-justify p-5 my-5"
+        >
           <h5 className=" font-weight-bold my-2 "> Our Story </h5>
           <p className="mb-4">
-            Brewer's Heaven was born in 2015 with a simple idea in mind: facilitate the access to
-            full-ﬂavoured and forward-thinking beers to as many people as possible. We’re committed
-            to building the ideal place to go to get our daily fix of brewing tips.
+            Brewer's Heaven was born in 2015 with a simple idea in mind:
+            facilitate the access to full-ﬂavoured and forward-thinking beers to
+            as many people as possible. We’re committed to building the ideal
+            place to go to get our daily fix of brewing tips.
           </p>
           <img
             src="https://images.unsplash.com/photo-1567579518027-7a113279f8ce?ixlib=
@@ -28,15 +33,20 @@ function About() {
             alt="Responsive image"
           />
         </div>
-      
-        <div className="ourApproachContainer col-sm-12 text-justify p-5 ml-3 my-5">
+
+        <div
+          id="cardTwo"
+          className="ourApproachContainer col-sm-12 text-justify p-5 my-5"
+        >
           <h5 className=" font-weight-bold my-2"> Our Approach To Beer </h5>
           <p className=" mb-4 ">
-            Our philosophy is to inform about the vast universe of craft beers and help our community grow!
-            We believe that beer is still vastly underestimated and that there's a beer for everyone, a whole
-            host of aromas, flavours, colours and textures to enjoy. Our mission is to get everyone on that
-            journey of discovery. Whether you are a beginner or a seasoned brewer we aim to help you make
-            the best beer possible.
+            Our philosophy is to inform about the vast universe of craft beers
+            and help our community grow! We believe that beer is still vastly
+            underestimated and that there's a beer for everyone, a whole host of
+            aromas, flavours, colours and textures to enjoy. Our mission is to
+            get everyone on that journey of discovery. Whether you are a
+            beginner or a seasoned brewer we aim to help you make the best beer
+            possible.
           </p>
           <img
             src="https://images.unsplash.com/photo-1535958636474-b021ee887b13?ixlib=rb-1.2.1&ixid=
@@ -45,13 +55,17 @@ function About() {
             alt="Responsive image"
           />
         </div>
-  
-        <div className="endResultContainer col-sm-12 text-justify p-5 ml-5 my-5">
+
+        <div
+          id="cardThree"
+          className="endResultContainer col-sm-12 text-justify p-5 my-5"
+        >
           <h5 className=" font-weight-bold my-2"> The End Result </h5>
           <p className="mb-4">
-            Whether you fancy a fruity, hoppy pale ale on a hot sunny day or an unctuous, satisfying stout
-            at the end of an evening - we've got you covered. Read on to discover some of the beers that await,
-            or book a brewery tour to see where the magic happens.
+            Whether you fancy a fruity, hoppy pale ale on a hot sunny day or an
+            unctuous, satisfying stout at the end of an evening - we've got you
+            covered. Read on to discover some of the beers that await, or book a
+            brewery tour to see where the magic happens.
           </p>
           <img
             src="https://images.unsplash.com/photo-1539097463168-fdc97aa4d984?ixlib=rb-1.2.
@@ -60,13 +74,15 @@ function About() {
             alt="Responsive image"
           />
         </div>
-
-        <button
-          className="btn-lg btn-info my-5"
-          onClick={redirect}
-        >
-          EXPLORE OUR RECIPES
-        </button>
+        <div className="buttonContainer">
+          <button
+            id="buttonExploreOurBeers"
+            className="btn-lg btn-info mx-auto"
+            onClick={redirect}
+          >
+            EXPLORE OUR BEERS
+          </button>
+        </div>
       </div>
     </div>
   );
